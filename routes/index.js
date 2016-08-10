b1965eda5947626a3ddfd9f5ac8e64afeb38c148
jav8586 <gerardo_jav_8@hotmail.com>
2016-08-10 17:28:05 -0500

    servidor

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/mensaje', function (req, res) {
+    res.render('mensaje', { title: 'Express' });
+});
+
+router.get('/mensajesevento', function (req, res) {
+    res.render('mensajesevento', { title: 'Express' });
+});
+
+
+module.exports = router;
